58af03da8b99bbfb3219c75d253cfead55370462
tamybl <tamara.bravo.l@gmail.com>
2017-11-05 21:41:57 -0300

    Version 0.0.2 Deshabilitar Boton Twit con espacios, 0 caracteres y contar de forma regresiva

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,36 @@
+// Para verificar los sucesos en el Textarea, se crea una funcion que va contando el numero de caracteres. Esta se ejecuta cada vez que algo cambia en el textarea.
+// Variable globales
+var count = 0;
+var maxLength = 140;
+// Boton inicia deshabilitado
+document.getElementById("btn-tweet").disabled = true;
+
+// Funcion que se ejecuta cada vez que el usuario escribe en textarea
+function counter () {
+	// Almacenar el largo de la cadena escrita en textarea
+	var messageLength = document.getElementById('comment').value.length;
+	// Se almacena en variable global.
+	count = messageLength;
+	// Caracteres Restantes
+	maxLength = 140 - count;
+	document.getElementById('wordLength').innerHTML = maxLength;
+	if (count >= 1) { // Si hay al menos un caracter el boton se habilita
+		document.getElementById("btn-tweet").disabled = false;
+	}
+	if (count == 0 || /^\s+$/.test(document.getElementById('comment').value) || count > 140){ // Sino no hay caracteres o puros espacios 
+		document.getElementById("btn-tweet").disabled = true;
+	}
+	
+}
 
 function addComment() {
+	// Cuando se apreta el boton de añadir comentario se reinicia
+	maxLength = 140;
+	count = 0;
+	document.getElementById('wordLength').innerHTML = maxLength; 
+	// Boton se vuelve a deshabilitar
+	document.getElementById("btn-tweet").disabled = true; 
+
 	var comments = document.getElementById('comment').value; // 1. Tomar texto en el textarea (Cada cosa que se desee obtener desde html es con value)
 	// 2. Limpiar input textarea
 	document.getElementById('comment').value = '';
